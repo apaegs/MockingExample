914751a48016b83661aee9e21ee9454842addfd5
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-02-02 12:27:22 +0100

    Add test shouldBookRoomWhenRoomIsAvailable() in BookingSystemTest.java

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -1,11 +1,16 @@
 package com.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BookingSystemTest {
@@ -23,6 +28,37 @@ class BookingSystemTest {
     @BeforeEach
     void setUp() {
         bookingSystem = new BookingSystem(timeProvider, roomRepository, notificationService);
+    }
+
+    @Test
+    void shouldBookRoomWhenRoomIsAvailable() throws NotificationException {
+        LocalDateTime now = LocalDateTime.of(2026,2,2,10,0);
+        LocalDateTime start = now.plusDays(1);
+        LocalDateTime end = start.plusHours(2);
+
+        Room room = mock(Room.class);
+
+        when(timeProvider.getCurrentTime()).thenReturn(now);
+        when(roomRepository.findById("A1")).thenReturn(Optional.of(room));
+        when(room.isAvailable(start,end)).thenReturn(true);
+
+        boolean result = bookingSystem.bookRoom("A1", start, end);
+
+        assertThat(result).isTrue();
+        verify(room).addBooking(any());
+        verify(roomRepository).save(room);
+        verify(notificationService).sendBookingConfirmation(any());
+        
+    }
+
+    @Test
+    void shouldReturnFalseWhenRoomIsNotAvailable() {
+
+    }
+
+    @Test
+    void shouldReturnTrueIfNotificationFails() {
+        
     }
 
 }
